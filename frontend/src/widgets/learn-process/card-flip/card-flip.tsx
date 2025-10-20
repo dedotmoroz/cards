2948a9e382b6fad27460e11f3b54893dd8781373
dedotmoroz <neff@yandex.ru>
2025-10-20 22:43:20 +0300

    css

--- frontend/src/widgets/learn-process/card-flip/card-flip.tsx
+++ frontend/src/widgets/learn-process/card-flip/card-flip.tsx
@@ -1,6 +1,6 @@
-import { forwardRef } from 'react';
-import { Box } from '@mui/material';
-import { CardBox } from './card-box.tsx'
+import {forwardRef} from 'react';
+import {Box} from '@mui/material';
+import {CardBox} from './card-box.tsx'
 
 interface CardFlipProps {
     question: string;
@@ -8,10 +8,6 @@ interface CardFlipProps {
     showAnswer: boolean;
     onClick?: () => void;
     className?: string;
-    onMouseDown?: (event: React.MouseEvent<HTMLDivElement>) => void;
-    onMouseMove?: (event: React.MouseEvent<HTMLDivElement>) => void;
-    onMouseUp?: (event: React.MouseEvent<HTMLDivElement>) => void;
-    onMouseLeave?: (event: React.MouseEvent<HTMLDivElement>) => void;
     onTouchStart?: (event: React.TouchEvent<HTMLDivElement>) => void;
     onTouchMove?: (event: React.TouchEvent<HTMLDivElement>) => void;
     onTouchEnd?: (event: React.TouchEvent<HTMLDivElement>) => void;
@@ -28,10 +24,6 @@ export const CardFlip = forwardRef<HTMLDivElement, CardFlipProps>(
        showAnswer,
        onClick,
        className,
-       onMouseDown,
-       onMouseMove,
-       onMouseUp,
-       onMouseLeave,
        onTouchStart,
        onTouchMove,
        onTouchEnd,
@@ -41,54 +33,42 @@ export const CardFlip = forwardRef<HTMLDivElement, CardFlipProps>(
        onPointerLeave,
   }, ref) => {
 
-      console.log('showAnswer === ', showAnswer);
+        return (
+            <Box
+                sx={{
+                    position: 'relative',
+                    height: 400,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
 
-      return (
-          <Box
-              sx={{
-                  position: 'relative',
-                  height: 400,
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  perspective: '1000px'
-              }}
+                onClick={onClick}
+                className={className}
+                onTouchStart={onTouchStart}
+                onTouchMove={onTouchMove}
+                onTouchEnd={onTouchEnd}
+                onPointerDown={onPointerDown}
+                onPointerMove={onPointerMove}
+                onPointerUp={onPointerUp}
+                onPointerLeave={onPointerLeave}
+            >
+                <Box
+                    ref={ref}
+                    sx={{
+                        position: 'relative',
+                        width: '100%',
+                        maxWidth: 500,
+                        height: 300,
+                        cursor: 'pointer',
+                    }}
 
-              onClick={onClick}
-              className={className}
-              onMouseDown={onMouseDown}
-              onMouseMove={onMouseMove}
-              onMouseUp={onMouseUp}
-              onMouseLeave={onMouseLeave}
-              onTouchStart={onTouchStart}
-              onTouchMove={onTouchMove}
-              onTouchEnd={onTouchEnd}
-              onPointerDown={onPointerDown}
-              onPointerMove={onPointerMove}
-              onPointerUp={onPointerUp}
-              onPointerLeave={onPointerLeave}
-          >
-              <Box
-                  ref={ref}
-                  sx={{
-                      position: 'relative',
-                      width: '100%',
-                      maxWidth: 500,
-                      height: 300,
-                      cursor: 'pointer',
-                      inset: 0,
-                      transformStyle: 'preserve-3d',
-                      transition: 'transform 600ms ease',
-                      transform: showAnswer ? 'rotateY(180deg)' : 'rotateY(0deg)',
-                      willChange: 'transform',
-                  }}
-
-              >
-                          <CardBox>
-                              {showAnswer ? question : answer}
-                          </CardBox>
-              </Box>
-          </Box>
-      );
-  }
+                >
+                    <CardBox>
+                        {showAnswer ? question : answer}
+                    </CardBox>
+                </Box>
+            </Box>
+        );
+    }
 );
