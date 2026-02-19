e55896a404f2d0f87ac0ee2e3bb98c769b6df165
dedotmoroz <neff@yandex.ru>
2026-02-19 23:15:04 +0300

    strapi /cms

--- frontend/src/shared/api/pagesApi.ts
+++ frontend/src/shared/api/pagesApi.ts
@@ -1,6 +1,6 @@
-const hostname = typeof window !== 'undefined' ? window.location.hostname : 'localhost';
-const isLocalhost = hostname === 'localhost' || hostname === '127.0.0.1';
-const STRAPI_URL = isLocalhost ? 'http://localhost:1337' : '/cms';
+// const hostname = typeof window !== 'undefined' ? window.location.hostname : 'localhost';
+// const isLocalhost = hostname === 'localhost' || hostname === '127.0.0.1';
+const STRAPI_URL = '/cms';
 
 export async function getPage(locale: string, slug: string) {
     const res = await fetch(
